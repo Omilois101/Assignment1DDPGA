a107db8a272e226fd018c51a7b8104bcdaa8f94e
Omilois101 <79018342+Omilois101@users.noreply.github.com>
2022-02-10 21:03:21 +0000

    Update tb_pattern_recog.vhd

--- Assignment1DDPGA/codeArchive/task6.3I/tb_pattern_recog.vhd
+++ Assignment1DDPGA/codeArchive/task6.3I/tb_pattern_recog.vhd
@@ -1,113 +1,35 @@
---------------------------------------------
--- Implementation of a Pattern Detector
--- 
---------------------------------------------
+ENTITY tb_recog2 IS END;
 
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-
-ENTITY recog2 IS
-PORT(
+ARCHITECTURE behav OF tb_recog2 IS
+  COMPONENT recog2 
+    port (
       X: in	bit;
       CLK: in	bit;
       RESET: in	bit;
       Y: out		bit);
-end;
-
-ARCHITECTURE myArch of recog2 is 
---------------------------------------------------------------------
--- This architecture us assuming that there is no overlapping or 
--- and there is a reseting of signal after the output has been asserted.
--- meaning previous iput variables after output assertion will not be read. 
----------------------------------------------------------------------------
-  TYPE state_type IS (resets,zero_state,one_state,out_state);
-  signal curState,nextState: state_type;
-  signal counter_sig : integer; 
-  signal en_counter,rest_counter: bit := '0';
-  Signal x_reg:STD_ULOGIC := '0';
-Begin 
+  END COMPONENT;
   
-  comb_nextstate: process(curState,x_reg,counter_sig)
-  BEGIN
-      CASE curState IS
-         WHEN resets =>
-            if x_reg = '0' then
-              nextState <= zero_state;
-            else
-               nextState <= resets; 
-            end if;
-        
-         WHEN zero_state => 
-            if x_reg= '0' then 
-                nextState <= zero_state;
-                if counter_sig < 14 then 
-                    en_counter <= '1';
-                end if;
-            end if;
-            if x_reg= '1' then 
-                 if counter_sig = 14 then 
-                    nextState <= one_state;
-                  ELSE 
-                    nextState <= resets; 
-                    rest_counter <= '1';
-                  end if;
-            end if;
-      
-         WHEN one_state =>
-            if x_reg= '1' then 
-                nextState <= one_state;
-                if counter_sig < 16 then 
-                  en_counter <= '1';
-               end if;
-            end if;
-            if x_reg= '1' then 
-                if counter_sig = 16 then 
-                    nextState <= out_state;
-                ELSE 
-                    nextState <= resets; 
-                    rest_counter <= '1';
-                end if;
-            end if;
-      
-         WHEN out_state =>
-             nextState <= resets; 
-      end case;  
-  end process;
+  FOR r_myArch: recog2 USE ENTITY WORK.recog2(myArch);
+  
+  SIGNAL x, reset, clk: bit :='0';
+  SIGNAL y: bit;
+  CONSTANT clk_period_half: time := 5 ns;
+  --CONSTANT no_of_cycles: integer := 4; -- no of cycles for result to be valid
+BEGIN
+  --generate Clk
+  clk <= NOT Clk AFTER clk_period_half WHEN NOW < 3 us ELSE clk; 
+  x <= '0', '1' AFTER 12 ns, '0' AFTER 24 ns, '1' AFTER 100 ns, '0' AFTER 120 ns, '1' AFTER 130 ns,
+    '0' AFTER 140 ns,'1' AFTER 150 ns, 
+    '0' AFTER 180 ns,
+    '1' AFTER 360 ns,
+    '0' AFTER 550 ns,
+    '1' AFTER 700 ns,
+    '0' AFTER 870 ns;
+    
+  reset <= '0' AFTER 5 ns,
+    '1' AFTER 10 ns;
+   
+   r_myArch: recog2 PORT MAP(x,clk,reset,y);
 
-  comb_out:process(curState)
-     begin 
-       y <= '0';  
-       IF curState = out_state THEN
-          y <= '1';
-       END IF;
-     end process;
-
-  SEQ_STATE: PROCESS (clk , reset)
-   BEGIN
-     if reset = '0' then 
-         curState <= resets;
-      elsif clk'event and clk = '1' then 
-         curState <= nextState;
-     END IF;    
-   END process;
-
-   counter:process(reset,en_counter,rest_counter, clk)
-      begin
-         if reset = '0' or rest_counter = '1'then 
-           counter_sig <= 0; 
-          elsif clk'event and clk = '1' then 
-             if en_counter = '1' then
-                counter_sig <= counter_sig + 1;
-             end if;
-         end if;
-      end process; 
-
-   registers: process(clk)
-	    BEGIN
-	      IF clk'event AND clk='1' THEN
-	        x_reg <= x;
-	      END IF;
-	    END PROCESS;
- 
-END myArch;
-
+    
+END;
