6b34074f15b92763d693014b9f3e98c9c57e2a10
Omilois101 <79018342+Omilois101@users.noreply.github.com>
2022-02-10 19:26:50 +0000

    Update mealy.vhd

--- Assignment1DDPGA/codeArchive/task5.2G/mealy.vhd
+++ Assignment1DDPGA/codeArchive/task5.2G/mealy.vhd
@@ -13,38 +13,49 @@ PORT(
   y: out STD_ULOGIC);
 end;
 
+----------------------------------------------------
+-- MEALY --
+-----------------------------------------------------
 ARCHITECTURE arch_mealy OF recog1 IS
   -- State declaration
-  TYPE state_type IS (INIT, FIRST ....);  -- List your states here 	
+  TYPE state_type IS (INIT,FIRST,SECOND,THIRD);  -- List your states here 	
   SIGNAL curState, nextState: state_type;
+  Signal x_reg:STD_ULOGIC := '0';
 BEGIN
   -----------------------------------------------------
-  combi_nextState: PROCESS(curState, x)
+  combi_nextState: PROCESS(curState, x_reg)
   BEGIN
     CASE curState IS
-      WHEN INIT =>
-        IF x='0' THEN 
+      WHEN INIT => 
+        IF x_reg='0' THEN 
+          nextState <= INIT;
+        ELSE
           nextState <= FIRST;
-        ELSE
-          ...... -- Fill in
-          ......
         END IF;
-        
       WHEN FIRST =>
-        IF x='0' THEN
-          nextState <= ? -- Fill in
+        IF x_reg='0' THEN
+          nextState <= SECOND;
         ELSE
-          ............? -- Fill in
+          nextState <= FIRST;
         END IF;
-      WHEN ............ -- Fill in
-        ..............
+      WHEN SECOND =>
+        IF x_reg='0' THEN
+          nextState <= INIT;
+        ELSE
+         nextState <= THIRD;         
+        END IF;
+      WHEN THIRD =>
+        IF x_reg ='0' THEN
+          nextState <= INIT;
+        ELSE
+          nextState <= FIRST;
+        END IF;      
     END CASE;
   END PROCESS; -- combi_nextState
-  -----------------------------------------------------
-  combi_out: PROCESS(curState, x)
+  combi_out: PROCESS(curState, x_reg)
   BEGIN
     y <= '0'; -- assign default value
-    IF curState = THIRD AND x='1' THEN
+    IF curState = THIRD AND x_reg='1' THEN
       y <= '1';
     END IF;
   END PROCESS; -- combi_output
@@ -57,5 +68,83 @@ BEGIN
       curState <= nextState;
     END IF;
   END PROCESS; -- seq
+  register: PROCESS(clk)
+	  BEGIN
+	    IF clk'event AND clk='1' THEN
+	      x_reg <= x;
+	    END IF;
+	  END PROCESS;
   -----------------------------------------------------
-END; -- arch_mealy
+END; -- arch_mealy  
+  
+  
+----------------------------------------------------
+-- MOORE --
+-----------------------------------------------------
+ARCHITECTURE arch_moore OF recog1 IS
+  -- State declaration
+  TYPE state_type IS (INIT,FIRST,SECOND,THIRD,FOURTH);  -- List your states here 	
+  SIGNAL curState, nextState: state_type;
+BEGIN
+  -----------------------------------------------------
+  combi_nextState: PROCESS(curState, x)
+  BEGIN
+    CASE curState IS
+      WHEN INIT => 
+        IF x='0' THEN 
+          nextState <= INIT;
+        ELSE
+          nextState <= FIRST;
+        END IF;
+        
+      WHEN FIRST =>
+        IF x='0' THEN
+          nextState <= SECOND;
+        ELSE
+          nextState <= FIRST;
+        END IF;
+      WHEN SECOND =>
+        IF x='0' THEN
+          nextState <= INIT;
+        ELSE
+         nextState <= THIRD;         
+        END IF;
+      WHEN THIRD =>
+        IF x='0' THEN
+          nextState <= FOURTH;
+        ELSE
+          nextState <= FIRST;
+        END IF;
+      WHEN FOURTH =>
+        IF x='0' THEN
+          nextState <= INIT;
+        ELSE
+          nextState <= FIRST;
+        END IF;   
+    END CASE;
+  END PROCESS; -- combi_nextState
+  combi_out: PROCESS(curState, x)
+  BEGIN
+    y <= '0'; -- assign default value
+    IF curState = FOURTH THEN
+      y <= '1';
+    END IF;
+  END PROCESS; -- combi_output
+  -----------------------------------------------------
+  seq_state: PROCESS (clk, reset)
+  BEGIN
+    IF reset = '0' THEN
+      curState <= INIT;
+    ELSIF clk'EVENT AND clk='1' THEN
+      curState <= nextState;
+    END IF;
+  END PROCESS; -- seq
+  
+  
+  -----------------------------------------------------
+END; -- arch_moore  
+  
+-----------------------------------------------------
+  
+
+
